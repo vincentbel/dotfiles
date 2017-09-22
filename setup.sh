8da0c549b143de8a8315e7f8811ef08ba4c72c87
VincentBel <buaazqh@gmail.com>
2017-09-23 03:23:04 +0800

    fix setup file

--- setup.sh
+++ setup.sh
@@ -1,20 +1,29 @@
 # backup first
 ./backup.sh
 
-# use git clone to ensure antigen can self update
-# https://github.com/zsh-users/antigen#antigen-selfupdate
-git clone https://github.com/zsh-users/antigen ~/.zsh
-
 # set up symlink
 ./symlinks.sh
 
+# make dir if needed
+mkdir -p ~/.zsh
+
+# use git clone to ensure antigen can self update
+# https://github.com/zsh-users/antigen#antigen-selfupdate
+git clone https://github.com/zsh-users/antigen ~/.zsh/antigen
+
+# change default shell to zsh
+chsh -s /bin/zsh
+
 # enter zsh shell to use the current .zshrc config
+# or open a new shell
 zsh
 
 ################## Node.js ###########################
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+
+# open a new shell
 
 # install node.js version 8.x
 nvm install 8
