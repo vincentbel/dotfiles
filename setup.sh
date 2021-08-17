a38e5e777469dfa1e8721aecd71604258e0c1ce5
Vincent Bel <buaazqh@gmail.com>
2021-08-17 06:50:42 +0000

    install antigen in github codespace

--- setup.sh
+++ setup.sh
@@ -3,6 +3,14 @@
 ########## Set up file for GitHub Codespace ##########
 # See more at: <https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles>
 
+printf "\n\n========== Installing antigen ==========\n\n"
+# make dir if needed
+mkdir -p ~/.zsh
+
+# use git clone to ensure antigen can self update
+# https://github.com/zsh-users/antigen#antigen-selfupdate
+curl -L git.io/antigen > ~/.zsh/antigen.zsh
+
 printf "\n\n========== Installing fnm ==========\n\n"
 # install fnm: https://github.com/Schniz/fnm
 curl -fsSL https://fnm.vercel.app/install | bash
