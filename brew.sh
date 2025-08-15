9f5d50af1e41d9d670ea7585b72e9442dcfa82ed
zhangqinghong.h <zhangqinghong.h@bytedance.com>
2025-08-15 11:44:46 +0800

    chore: update brew.sh

--- brew.sh
+++ brew.sh
@@ -35,10 +35,10 @@ brew install neovim
 # https://github.com/Schniz/fnm
 brew install Schniz/tap/fnm
 
-fnm install 12
+fnm install 22
 
 # install global npm dependencies
-./npm.sh
+# ./npm.sh
 
 brew install navi
 
@@ -46,40 +46,37 @@ brew install navi
 # Brew cask time!
 ############################################
 
-brew install homebrew/cask-cask
-brew tap homebrew/cask-versions
-
 
 # daily
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install firefox
-brew cask install google-backup-and-sync
+# brew cask install google-chrome
+brew install --cask google-chrome-canary
+brew install --cask firefox
+# brew cask install google-backup-and-sync
 # brew cask install rescuetime
-brew cask install iina # video player
+# brew cask install iina # video player
 # brew cask install dropbox
 # install from App Store
 # brew cask install evernote
 # brew cask install neteasemusic
-brew cask install sogouinput
+# brew cask install sogouinput
 
 # dev
-brew cask install iterm2
+brew install --cask iterm2
 # brew cask install hyper
 # brew cask install alfred
 # brew cask install dash
-brew cask install atom
-brew cask install visual-studio-code
-brew cask install sublime-text
-brew cask install gitup
-brew cask install sketch
+# brew cask install atom
+brew install --cask visual-studio-code
+brew install --cask sublime-text
+# brew cask install gitup
+# brew cask install sketch
 # brew cask install nestopia # nes games
 # brew cask install charles
 # brew cask install docker
 # brew cask install jetbrains-toolbox
 
 # https://github.com/egoist/devdocs-desktop
-brew cask install devdocs
+brew install --cask devdocs
 
 # https://github.com/federico-terzi/espanso
 brew tap federico-terzi/espanso
@@ -88,23 +85,23 @@ espanso install all-emojis
 
 # quick look plugin
 # <https://github.com/sindresorhus/quick-look-plugins>
-brew cask install qlcolorcode
-brew cask install qlstephen
-brew cask install qlmarkdown
-brew cask install quicklook-json
-brew cask install betterzip
-brew cask install qlimagesize
-brew cask install webpquicklook
+brew install --cask qlcolorcode
+brew install --cask qlstephen
+brew install --cask qlmarkdown
+brew install --cask quicklook-json
+# brew cask install betterzip
+brew install --cask qlimagesize
+brew install --cask webpquicklook
 
 
 # tools
-brew cask install imageoptim # compress image
-brew cask install the-unarchiver # addtional unzipper for .7z .rar
+brew install --cask imageoptim # compress image
+brew install --cask the-unarchiver # addtional unzipper for .7z .rar
 # brew cask install keycastr # keystroke visualizer
 # brew cask install karabiner # custom key binding
-brew tap dteoh/sqa
-brew cask install slowquitapps # delay cmd+q
-defaults write com.dteoh.SlowQuitApps delay -int 700
+# brew tap dteoh/sqa
+# brew cask install slowquitapps # delay cmd+q
+# defaults write com.dteoh.SlowQuitApps delay -int 700
 
 # brew cask install shadowsocksx-ng # over the fucking wall
 
